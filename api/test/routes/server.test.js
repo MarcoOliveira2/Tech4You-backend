18a28b535e006038bb3d9350e7fecdf0556c0fd4
MarcoOliveira2 <fermandesmarco@gmail.com>
2021-11-30 20:21:46 +0000

    Folders & Files

---
+++ api/test/routes/server.test.js
@@ -0,0 +1,6 @@
+const supertest = require('supertest');
+
+const request = supertest('http://localhost:3001');
+test.skip('Validar se o servidor responde na porta 3001', () => {
+  return request.get('/').then((res) => expect(res.status).toBe(200));
+});
