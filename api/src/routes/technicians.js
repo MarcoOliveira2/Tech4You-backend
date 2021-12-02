afdb8c9bc639d2f2260bf034730e408652a9f072
miguelpinto128 <miguelpinto128@gmail.com>
2021-12-02 19:54:47 +0000

    create technician route

--- api/src/routes/technicians.js
+++ api/src/routes/technicians.js
@@ -1,11 +1,12 @@
 module.exports = (app) => {
   const findAll = (req, res) => {
-    app.db('technicians').select()
+    app.services.technician.findAll()
       .then((result) => res.status(200).json(result));
   };
 
   const create = async (req, res) => {
-    const result = await app.db('technicians').insert(req.body, '*');
+    const result = await app.services.technician.save(req.body);
+    if (result.error) return res.status(400).json(result);
     res.status(201).json(result[0]);
   };
 
