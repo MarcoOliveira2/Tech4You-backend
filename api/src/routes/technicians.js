9718630f98ad3b137e4d6e870afcddc7c7756d6a
MarcoOliveira2 <fermandesmarco@gmail.com>
2022-01-10 23:57:06 +0000

    Fix API - Find function

--- api/src/routes/technicians.js
+++ api/src/routes/technicians.js
@@ -19,7 +19,8 @@ module.exports = (app) => {
   });
 
   router.get('/:id', (req, res, next) => {
-    app.services.technician.findOne({ id: req.params.id })
+    console.log(req);
+    app.services.technician.find({ id: req.params.id })
       .then((result) => res.status(200).json(result))
       .catch((err) => next(err));
   });
