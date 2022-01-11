a7174a954c4c53de8aa2e0533f15476f0a29e878
miguelpinto128 <miguelpinto128@gmail.com>
2022-01-11 22:41:07 +0000

    Fix_Authentication

--- api/src/routes/technicians.js
+++ api/src/routes/technicians.js
@@ -19,7 +19,6 @@ module.exports = (app) => {
   });
 
   router.get('/:id', (req, res, next) => {
-    console.log(req);
     app.services.technician.find({ id: req.params.id })
       .then((result) => res.status(200).json(result))
       .catch((err) => next(err));
