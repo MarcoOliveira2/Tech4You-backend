f88e4e1c3b035e6997b0fa23445bc3c02403586a
miguelpinto128 <miguelpinto128@gmail.com>
2022-01-16 14:58:18 +0000

    Error messages

--- api/src/services/equipment.js
+++ api/src/services/equipment.js
@@ -1,5 +1,14 @@
+const ValidationError = require('../errors/validationError');
+
 module.exports = (app) => {
   const save = (equipment) => {
+    if (!equipment.typeEquipment) throw new ValidationError('O tipo de equipamento é um atributo obrigatório');
+    if (!equipment.serialNumber) throw new ValidationError('O número de série é um atributo obrigatório');
+    if (!equipment.brand) throw new ValidationError('A marca do equipamento é um atributo obrigatório');
+    if (!equipment.accessories) throw new ValidationError('Os acessórios são um atributo obrigatório');
+    if (!equipment.damages) throw new ValidationError('Os danos do equipamento são um atributo obrigatório');
+    if (!equipment.client_id) throw new ValidationError('O Id do cliente é um atributo obrigatório');
+
     return app.db('equipments').insert(equipment, '*');
   };
 
