5d6a2eca0f50f6b766729376093dcac2be39aba5
miguelpinto128 <miguelpinto128@gmail.com>
2022-01-16 21:34:27 +0000

    teste husky1

--- api/src/services/equipment.js
+++ api/src/services/equipment.js
@@ -11,6 +11,7 @@ module.exports = (app) => {
 
   const a = 1234;
   const b = 1234;
+  const c = 1234;
   const save = async (equipment) => {
     if (!equipment.typeEquipment) throw new ValidationError('O tipo de equipamento é um atributo obrigatório');
     if (!equipment.serialNumber) throw new ValidationError('O número de série é um atributo obrigatório');
